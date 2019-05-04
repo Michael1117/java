de2b110677531351822c658f4d510dcf6605b260
Michael1117 <1322720377@qq.com>
2019-05-04 22:26:47 +0800

    spring 2

---
+++ day02_04_account_annoioc_withoutxml/src/main/java/com/hefeng/dao/IAccountDao.java
@@ -0,0 +1,38 @@
+package com.hefeng.dao;
+
+import com.hefeng.domain.Account;
+
+import java.util.List;
+
+public interface IAccountDao {
+
+    /**
+     * 查询所有
+     * @return
+     */
+    List<Account> findAllAccount();
+
+    /**
+     * 查询一个
+     * @return
+     */
+    Account findAccountById(Integer accountId);
+
+    /**
+     * 保存
+     * @param account
+     */
+    void saveAccount(Account account);
+
+    /**
+     * 更新
+     * @param account
+     */
+    void updateAccount(Account account);
+
+    /**
+     * 删除
+     * @param accountId
+     */
+    void deleteAccount(Integer accountId);
+}
